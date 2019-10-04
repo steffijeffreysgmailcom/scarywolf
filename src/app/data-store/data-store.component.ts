68fc3b6832a9a075b11007b78d23b24a9df7a119
chris55miao <42607545+chris55miao@users.noreply.github.com>
2019-10-04 18:03:46 +1000

    Feature/displaying instruction (#4)
    * Display instructions for different turn

--- src/app/data-store/data-store.component.ts
+++ src/app/data-store/data-store.component.ts
@@ -1,27 +1,94 @@
-import { Component } from '@angular/core';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {Component} from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 
-
-
-export enum CharacterState{
+export enum CharacterState {
+  dead,
   alive,
-  dead
+}
+
+export enum Role {
+  Villager,
+  Wolf,
+  Witch,
+  Prophet,
+  Hunter
+}
+
+export class Character {
+  state: CharacterState = CharacterState.alive;
+
+  constructor(public name: String, public role: Role) {
+  }
+
+  killCharacter() {
+    this.state = CharacterState.dead;
+  }
+}
+
+export class Witch extends Character {
+
+  constructor(public name: String, public role: Role, public RessuranceWitchRule: String, bothRessurgancePoisin: boolean) {
+    super(name, role);
+  }
+}
+
+export class GameState {
+
+  currentState = GameStateEnum.closeEyeTurn;
+
+  NextState() {
+    this.currentState += 1;
+  }
+
+  CurrentState() {
+    return this.currentState;
+  }
+
+  CurrentInstruction() {
+    return GameInstructionEnum[GameStateEnum[this.currentState]];
+  }
+}
+
+export enum GameInstructionEnum {
+  'closeEyeTurn' = 'Everyone close your eyes',
+  'wolvesTurn' = 'Wolves open your eyes',
+  'witchTurn' = 'Wolves close your eyes, Witch open your eyes',
+  'prophetTurn' = 'Witch close your eyes, Prophet open your eyes',
+  'HunterTurn' = 'Prophet close your eyes, Hunter open your eyes'
+}
+
+export enum GameStateEnum {
+  'closeEyeTurn',
+  'wolvesTurn',
+  'witchTurn',
+  'prophetTurn',
+  'HunterTurn'
 }
 
 @Injectable()
 export class DataStore {
-  currentCharacter:String = "Blah";
-  StoreCharacter(name: String) {
-    this.currentCharacter = name;
+
+  characters: Array<Character> = [];
+
+  StoreCharacter(character: Character) {
+    this.characters.push(character);
+    console.log('create');
   }
 
-  UpdateCharacter(state: CharacterState) {
-
+  GetAllCharacters(): Array<Character> {
+    return this.characters;
   }
 
-  GetCharacter() {
-    return this.currentCharacter
+  GetCharacterByName(name: String): Character {
+    var character: Character = null;
+    this.characters.forEach((chara) => {
+      // TODO: comparing string using ===?
+      if (chara.name === name) {
+        character = chara;
+      }
+    });
+    return character;
   }
 
 }
--- src/app/game-play/game-play.component.ts
+++ src/app/game-play/game-play.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { DataStore } from "../data-store/data-store.component";
+import {Component, OnInit} from '@angular/core';
+import {Character, DataStore, GameInstructionEnum, GameState, GameStateEnum} from '../data-store/data-store.component';
 
 
 @Component({
@@ -8,17 +8,32 @@ import { DataStore } from "../data-store/data-store.component";
   styleUrls: ['./game-play.component.css']
 })
 export class GamePlayComponent implements OnInit {
-  player:String = "No idea";
-  constructor(private data: DataStore) { }
+  GameInstructionEnum = GameInstructionEnum;
+  GameStateEnum = GameStateEnum;
+  players: Array<Character>;
+  currentTurn = new GameState();
+
+  constructor(private data: DataStore) {
+  }
+
   ngOnInit() {
-    this.player = this.data.GetCharacter();
+    console.log('here');
+    this.players = this.data.GetAllCharacters();
+  }
+
+  SelectPersonToKill(name: String) {
+    this.data.GetCharacterByName(name).killCharacter();
   }
 
   StartGame() {
-    var audio = new Audio();
-    audio.src = "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3";
+    const audio = new Audio();
+    audio.src = 'https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3';
     audio.load();
     audio.play();
+  }
+
+  SwitchTurn() {
+    this.currentTurn.NextState();
   }
 
 }
